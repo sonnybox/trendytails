fd6a0fa5f7018486b3e54eec95908fe42e46dfe8
Sonny Box <sonny@sonnybox.com>
2024-03-12 14:16:41 -0700

    Added template env file to connect to a specific DB

---
+++ app/api/mariadb/route.ts
@@ -0,0 +1,32 @@
+'use server';
+
+import mariadb from 'mariadb';
+
+const pool = mariadb.createPool({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+});
+
+export async function POST(req: Request) {
+    const body = await req.json();
+    const conn = await pool.getConnection();
+    const data = await conn.query(body.query);
+    conn.release();
+    let response;
+
+    try {
+        if (body.queryonly == false) {
+            response = JSON.stringify(data);
+        } else {
+            response = JSON.stringify({});
+        }
+    } catch (error) {
+        console.log(`FYI an error was caught -> ${error}`);
+    }
+
+    return new Response(response, {
+        headers: { 'Content-Type': 'application/json' },
+    });
+}
